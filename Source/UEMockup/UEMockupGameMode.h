66c954e371356fbeae0109825d7da6f903899563
oscar <oscar.ken@gmail.com>
2024-12-30 16:18:20 +0800

    init proj

---
+++ Source/UEMockup/UEMockupGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEMockupGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUEMockupGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUEMockupGameMode();
+};
+
+
+
